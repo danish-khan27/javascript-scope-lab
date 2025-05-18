77cac7abefdfcba157b6a44bb868121d378f1d23
Danish <dkhans2001@gmail.com>
2025-05-17 20:53:10 -0500

    burger

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+// Step 1: Global Scope Variables
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Step 2: Function Scoped Variable and Array Modification
+function addBurger() {
+  const newBurger = 'Flatburger';
+  burgers.push(newBurger);
+}
+
+// Step 3: Block Scope and Another Function
+if(true) {
+  const anotherNewBurger = 'Maple Bacon Burger';
+  burgers.push(anotherNewBurger);
+
+  function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake';
+  }
+
+  window.changeFeaturedDrink = changeFeaturedDrink;
+}
